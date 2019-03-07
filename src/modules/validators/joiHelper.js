e73957342ea70992e90fbf346da5269314eac2e3
bcorre <benjamin.corre@systeme-u.fr>
2019-03-07 15:38:43 +0100

    feat(joiHelper): add helper (#5)

---
+++ src/modules/validators/joiHelper.js
@@ -0,0 +1,21 @@
+import Joi from 'joi'
+import { BusinessException } from '../exception/BusinessException'
+import { ErreurDO } from '../objects/ErreurDO'
+
+/**
+ * Validate an object with the Joi model
+ * @param {*} model joi
+ * @param {*} object to validate
+ * @return a valid object
+ */
+export default function check(model, object) {
+  const { error, value } = Joi.validate(object, model, { abortEarly: false })
+  if (error) {
+    const errors = error.details.map(({ message, context, type }) => {
+      const field = context.key
+      return new ErreurDO(field, `${field}.${type}`, message)
+    })
+    throw new BusinessException(errors)
+  }
+  return value
+}
