3f1731b6da62ecebd761284c0dc1b95e75cd829d
Leo Lienard <leo.lienard@systeme-u.fr>
2019-05-23 15:26:49 +0200

    Intégration des types jest dans le code

--- src/tests/expect.extend.ts
+++ src/tests/expect.extend.ts
@@ -20,3 +20,13 @@ expect.extend({
         }
     }
 })
+/// <reference types="jest" />
+declare namespace jest {
+    interface Matchers<R> {
+        /**
+         * Ensures that an array contains an object like argument
+         * @param argument
+         */
+        toContainObjectLike(argument: any): R
+    }
+}
